fb56b30739fbe564c483993ea56872f1e4f2fdbd
Tim J. Chang <tim.jtchang@gmail.com>
2024-12-17 21:39:30 -0700

    update latitude and longtitude

--- playground/sendURL.py
+++ playground/sendURL.py
@@ -1,10 +1,25 @@
 import requests
 
-url = 'http://127.0.0.1:5000/detect_url'
-image_path = '../samples/shit1.jpg'
+# Define the endpoint URL
+url = "http://127.0.0.1:5000/detect_url"
 
-with open(image_path, 'rb') as image_file:
-    files = {'file': ('image.jpg', image_file, 'image/jpeg')}
-    response = requests.post(url, files=files)
+# File path to test image
+file_path = "../samples/shit1.jpg"
 
-print(response.json())
+# Define the latitude and longitude
+latitude = 34.0522
+longitude = -118.2437
+
+# Prepare the payload
+files = {'file': open(file_path, 'rb')}
+data = {'latitude': latitude, 'longitude': longitude}
+
+# Send the POST request
+try:
+    response = requests.post(url, files=files, data=data)
+
+    # Print the response from the server
+    print("Response Status Code:", response.status_code)
+    print("Response Body:", response.json())
+except Exception as e:
+    print("An error occurred:", str(e))
